c745046287fef82733385b0e5241ed4228d079e4
Brody McCoy <79420042+mccoybrody@users.noreply.github.com>
2021-03-03 11:31:22 -0600

    calc

---
+++ forms/calc/calc.js
@@ -0,0 +1,15 @@
+function calculate(num1, num2) {
+        let answer = (num1 + num2)
+        return answer
+        }
+        
+let number1 = prompt("Choose a number.")
+let number2 = prompt("Choose another number.")
+
+let answer = calculate(number1, number2)
+console.log(`The sum of ${number1} and ${number2} is ${answer}`)
+        
+        
+        
+        
+        
---
+++ forms/for/for.js
@@ -0,0 +1,7 @@
+let names = ["Bob", "Janet", "Tom", "Eric", "Lucy", "Alyssa", "Dustin"]
+
+names.length
+
+for (i = 0; i < names.length; i++) 
+    console.log(names[i])
+    
